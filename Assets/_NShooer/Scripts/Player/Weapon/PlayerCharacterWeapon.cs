70162f41f6b24e82dd3663904c5511e6366fbe66
TechAoba <TechAoba@163.com>
2026-03-19 15:55:47 +0800

    换弹逻辑

--- Assets/_NShooer/Scripts/Player/Weapon/PlayerCharacterWeapon.cs
+++ Assets/_NShooer/Scripts/Player/Weapon/PlayerCharacterWeapon.cs
@@ -9,34 +9,71 @@ namespace NShooter
 	public class PlayerCharacterWeapon : NetworkBehaviour
 	{
 		[Header("Fire Config")]
-		[SerializeField] private float _fireRate = 0.1f;
-		[SerializeField] private Transform _muzzlePoint;	// 枪口位置
-		[SerializeField] private float _maxDistance = 100;	// 射程	
-		[SerializeField] private LayerMask _hitLayerMask;	
+		[SerializeField] private float _fireRate = 0.1f;		// 射速
+		[SerializeField] private Transform _muzzlePoint;		// 枪口位置
+		[SerializeField] private float _maxDistance = 100;		// 射程	
+		[SerializeField] private LayerMask _hitLayerMask;		// 击中Layer
+		[SerializeField] private int _maxAmmo = 30;				// 弹夹容量
+		[SerializeField] private float _reloadSpeed = 2f;		// 换弹速度
+		private float _lastShotTimeLocal; 	// 客户端本地记录（仅用于输入控制）
 
 		[Header("Server Data")]
-		[SerializeField] private float _lastShotTime;	// 上次射击时间（服务器维护）
+		[SerializeField] private float _lastShotTime;			// 上次射击时间（服务器维护）
+
+		[Header("Sync Data")]
+		[SyncVar(hook = nameof(OnAmmoChanged))]
+		private int _currentAmmoAmount;							// 当前子弹数
+		[SyncVar]
+		private bool _isReloading = false;						// 当前是否在装弹
 
 		[Header("Fire Data")]
-		[SerializeField] private VFXBulletVisual _bulletPrefab;
-		[SerializeField] private GameObject _vfxMuzzleFlash;
-		[SerializeField] private float _muzzleFlashDuration = 0.05f;
+		[SerializeField] private VFXBulletVisual _bulletPrefab;		// 子弹GO
+		[SerializeField] private GameObject _vfxMuzzleFlash;		// 枪口火花GO
+		[SerializeField] private float _muzzleFlashDuration = 0.05f;// 火花持续时间
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+			_currentAmmoAmount = _maxAmmo;
+        }
 
         private void Start()
         {
             _vfxMuzzleFlash.SetActive(false);
         }
 
-        private void FixedUpdate()
+        private void Update()
         {
 			if (!isLocalPlayer) return;
-			if (Input.GetKey(KeyCode.Mouse0))
+			if (Input.GetMouseButton(0) && CanShotLocally())
 			{
-				// 鼠标左键 为射击
+				// 鼠标左键 射击（向服务器发送射击命令）
 				CmdShooting();
+				_lastShotTimeLocal = Time.time; // 更新本地时间
 			}
-			
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				CmdReload();
+			}
         }
+		private bool CanShotLocally()
+		{
+			if (_isReloading || _currentAmmoAmount <= 0) 
+				return false;
+				
+			return (Time.time - _lastShotTimeLocal) >= _fireRate;
+		}
+
+		// 服务器执行换弹请求
+		[Command]
+		private void CmdReload()
+		{
+			// 服务器验证是否需要 换弹
+			if (_isReloading || _currentAmmoAmount >= _maxAmmo)
+				return;
+			
+			ServerStartReload();
+		}
 
 		// 服务器验证并执行射击
 		[Command]
@@ -44,6 +81,11 @@ namespace NShooter
 		{
 			if (!CanShoot()) return;
 			_lastShotTime = Time.time;
+			// 弹夹弹药打空
+			if (--_currentAmmoAmount <= 0)
+			{
+				ServerStartReload();
+			}
 
 			// 服务器执行射线检测
 			Vector3 origin = _muzzlePoint.position;
@@ -70,8 +112,26 @@ namespace NShooter
 		}
 
 		[Server]
+		private void ServerStartReload()
+		{
+			_isReloading = true;
+			StartCoroutine(ReloadCoroutine());
+		}
+
+		[Server]
+		private IEnumerator ReloadCoroutine()
+		{
+			yield return new WaitForSeconds(_reloadSpeed);
+			_currentAmmoAmount = _maxAmmo;
+			_isReloading = false;
+		}
+
+		[Server]
 		private bool CanShoot()
 		{
+			// 正在填充弹药
+			if (_isReloading || _currentAmmoAmount <= 0) return false;
+			// 射击间隔
 			float currentTime = Time.time;
 			return (currentTime - _lastShotTime) >= _fireRate;
 		}
@@ -102,6 +162,12 @@ namespace NShooter
 			_vfxMuzzleFlash.SetActive(true);
 			yield return new WaitForSeconds(_muzzleFlashDuration);
 			_vfxMuzzleFlash.SetActive(false);
+		}
+
+		// 玩家向服务器发出换弹请求。所有客户端改变子弹数量，并执行 换弹逻辑/动画
+		private void OnAmmoChanged(int oldAmmo, int newAmmo)
+		{
+			
 		}
     }
 }
